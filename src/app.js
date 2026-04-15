d28392aa8531cd9ff285366fe1864c731ad18618
Ramitha <ramithahs17@gmail.com>
2026-04-15 21:53:32 +0530

    elva page

--- src/app.js
+++ src/app.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const path = require('path');
 const routes = require('./routes');
 
 const app = express();
 
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '../public')));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/index.html'));
+});
 app.use(routes);
 app.use((err, req, res, next) => {
   console.error(err);
